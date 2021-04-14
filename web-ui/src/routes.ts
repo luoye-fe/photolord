4dbc1c714c440abf73afbc1b130ad8eccfacc42d
luoye <luoyefe@gmail.com>
2021-04-14 18:39:19 +0800

    feat: login route

--- web-ui/src/routes.ts
+++ web-ui/src/routes.ts
@@ -1,10 +1,12 @@
 import Home from '@/pages/Home';
+import Login from '@/pages/Login';
 
-const routerConfig = [
-  {
-    path: '/',
-    component: Home,
-  },
-];
+const routerConfig = [{
+  path: '/',
+  component: Home,
+}, {
+  path: '/login',
+  component: Login,
+}];
 
 export default routerConfig;
