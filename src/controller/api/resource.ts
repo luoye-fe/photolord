e4c3f7adccdbe6d510ad286a30c9c1e19b7f2f48
luoye <luoyefe@gmail.com>
2021-04-16 08:49:22 +0800

    feat: transcode api

--- src/controller/api/resource.ts
+++ src/controller/api/resource.ts
@@ -1,5 +1,3 @@
-import * as path from 'path';
-import sharp from 'sharp';
 import { Controller, Get, Inject, Provide } from '@midwayjs/decorator';
 import { InjectEntityModel } from '@midwayjs/orm';
 import { Repository } from 'typeorm';
@@ -49,46 +47,5 @@ export class ApiResourceController {
       hasMore: count > page * size,
       list: result.map(this.resourceService.formatResourceInfo),
     });
-  }
-
-  @Get('/transcode')
-  async transcode(ctx: Context) {
-    const { md5, width, height } = ctx.query;
-    if (!md5) return ctx.fail(400, ctx.errorCode.Params_Error);
-
-    const { path: filePath, library_id: libraryId } = await this.resourceModel.findOne({ md5 });
-
-    const { path: libraryPath } = await this.libraryModel.findOne({
-      id: libraryId,
-    });
-
-    const fileAbsolutePath = path.join(libraryPath, filePath);
-    const resizeOptions: sharp.ResizeOptions = {};
-
-    if (width && !height) {
-      resizeOptions.width = Number(width);
-      resizeOptions.fit = 'contain';
-    }
-
-    if (!width && height) {
-      resizeOptions.height = Number(height);
-      resizeOptions.fit = 'contain';
-    }
-
-    if (width && height) {
-      resizeOptions.width = Number(width);
-      resizeOptions.height = Number(height);
-    }
-
-    if (!width && !height) {
-      resizeOptions.width = 300;
-      resizeOptions.fit = 'contain';
-    }
-
-    ctx.response.type = 'image/jpeg';
-    ctx.body = await sharp(fileAbsolutePath)
-      .resize(resizeOptions)
-      .toFormat('jpeg', { force: true })
-      .toBuffer();
   }
 }
---
+++ src/controller/api/transcode.ts
@@ -0,0 +1,60 @@
+import * as path from 'path';
+import sharp from 'sharp';
+import { Controller, Get, Provide } from '@midwayjs/decorator';
+import { InjectEntityModel } from '@midwayjs/orm';
+import { Repository } from 'typeorm';
+
+import { LibraryModel } from '@/entity/library';
+import { ResourceModel } from '@/entity/resource';
+import { Context } from 'node:vm';
+
+@Provide()
+@Controller('/api/transcode')
+export class ApiTranscodeController {
+  @InjectEntityModel(LibraryModel)
+  libraryModel: Repository<LibraryModel>;
+
+  @InjectEntityModel(ResourceModel)
+  resourceModel: Repository<ResourceModel>;
+
+  @Get('/')
+  async transcode(ctx: Context) {
+    const { md5, width, height } = ctx.query;
+    if (!md5) return ctx.fail(400, ctx.errorCode.Params_Error);
+
+    const { path: filePath, library_id: libraryId } = await this.resourceModel.findOne({ md5 });
+
+    const { path: libraryPath } = await this.libraryModel.findOne({
+      id: libraryId,
+    });
+
+    const fileAbsolutePath = path.join(libraryPath, filePath);
+    const resizeOptions: sharp.ResizeOptions = {};
+
+    if (width && !height) {
+      resizeOptions.width = Number(width);
+      resizeOptions.fit = 'contain';
+    }
+
+    if (!width && height) {
+      resizeOptions.height = Number(height);
+      resizeOptions.fit = 'contain';
+    }
+
+    if (width && height) {
+      resizeOptions.width = Number(width);
+      resizeOptions.height = Number(height);
+    }
+
+    if (!width && !height) {
+      resizeOptions.width = 300;
+      resizeOptions.fit = 'contain';
+    }
+
+    ctx.response.type = 'image/jpeg';
+    ctx.body = await sharp(fileAbsolutePath)
+      .resize(resizeOptions)
+      .toFormat('jpeg', { force: true })
+      .toBuffer();
+  }
+}
