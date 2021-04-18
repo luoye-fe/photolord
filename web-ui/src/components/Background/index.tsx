cbc80ff8e2f886fc30e5051d6400ece838a386cc
luoye <luoyefe@gmail.com>
2021-04-18 22:08:13 +0800

    feat: bg style adjust

--- web-ui/src/components/Background/index.tsx
+++ web-ui/src/components/Background/index.tsx
@@ -9,10 +9,10 @@ export default function Background() {
     const pattern = trianglify({
       width: window.innerWidth,
       height: window.innerHeight,
-      xColors: ['#543005', '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#f5f5f5', '#c7eae5', '#80cdc1', '#35978f', '#01665e', '#003c30'],
-      // xColors: ['#ddd', '#333'],
+      // xColors: ['#543005', '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#f5f5f5', '#c7eae5', '#80cdc1', '#35978f', '#01665e', '#003c30'],
+      xColors: ['#999', '#333'],
       variance: 1,
-      cellSize: '30',
+      cellSize: '60',
     });
 
     pattern.toCanvas(canvasRef.current);
